f64902b90a1294657801495fa3be26152f835d5c
leonardorsg <95626833+leonardorsg@users.noreply.github.com>
2023-10-09 18:29:24 +0100

    Create Class.h

---
+++ headers/Class.h
@@ -0,0 +1,33 @@
+#include <vector>
+#include "Uc.h"
+using namespace std;
+
+class Class
+{
+public:
+    Class();
+    Class(Uc ucCode,string classCode, string weekDay, double startHour, double duration, string type);
+    Class(string ucCode,string classCode, string weekDay, double startHour, double duration, string type);
+    Uc getUc();
+    string getClass();
+    void setUc(const Uc &uc);
+    void addStudent(Student student);
+    void removeStudent(Student student);
+    void addStudent(string studentCode, string studentName);
+    vector<Student> getStudents();
+
+
+private:
+    Uc uc;
+    vector<Student> students;
+    string weekDay;
+    double startHour;
+    double duration;
+    string type;
+
+};
+
+
+#endif //AED_PROJECT_CLASS_H
+
+
