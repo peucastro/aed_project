0d2d290ff6d820b0aededa4fa0c777bd92ce7f72
marcelmedeiros1 <marcel_segundo@outlook.com>
2023-10-09 18:26:39 +0100

    Student completed

--- src/Student.cpp
+++ src/Student.cpp
@@ -6,9 +6,22 @@
 
 Student::Student() {
     this->studentName = "NO_NAME";
-    this->studentCode = "";
+    this->studentCode = "NO_CODE";
 }
 Student::Student(const string &studentCode, const string &studentName) {
     this->studentCode = studentCode;
     this->studentName = studentName;
 }
+ string Student::getstudentCode(){
+    return this->studentCode;
+ }
+ void Student::setstudentCode(const string &studentCode){
+    this->studentCode = studentCode;
+ }
+ string Student::getstudentName(){
+    return this->studentName;
+ }
+ void Student::setstudentName(const string &studentName){
+    this->studentName = studentName;
+ }
+
