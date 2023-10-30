a696bcbec43dfe8127f41a53d391411a1269d1c6
Pedro Castro <falecompedroac@gmail.com>
2023-10-30 14:53:24 +0000

    adicionei a estrutura do app

---
+++ inc/App.hpp
@@ -0,0 +1,24 @@
+#ifndef AED_PROJECT_APP_H
+#define AED_PROJECT_APP_H
+
+#include "Request.hpp"
+#include <cstdlib>
+
+void clearScreen()
+{
+#ifdef _WIN32
+    std::system("cls");
+#else
+    std::system("clear");
+#endif
+}
+
+class App
+{
+public:
+    void mainMenu();
+    void studentMenu(std::string studentCode);
+    void adminMenu();
+};
+
+#endif
---
+++ src/App.cpp
@@ -0,0 +1,110 @@
+#include "../inc/App.hpp"
+using namespace std;
+
+void App::mainMenu()
+{
+    cout << "Welcome to the L.EIC Schedule Management System" << endl
+         << endl;
+
+    int option;
+    cout << "Enter your option:" << endl
+         << "[1] Student" << endl
+         << "[2] Administratot" << endl;
+    cin >> option;
+
+    clearScreen();
+
+    switch (option)
+    {
+    case 1:
+    {
+        string studentCode;
+        cout << "Enter your student code: " << endl, cin >> studentCode;
+        studentMenu(studentCode);
+        break;
+    }
+    case 2:
+    {
+        string login, pass;
+        cout << "Enter your login: ", cin >> login, cout << endl;
+        cout << "Enter your password: ", cin >> pass, cout << endl;
+
+        if (login == "adm" && pass == "123")
+            adminMenu();
+        else
+        {
+            cout << "Wrong credentials, try again" << endl;
+            mainMenu();
+        }
+        break;
+    }
+    }
+}
+
+void App::studentMenu(string studentCode)
+{
+    cout << "Student menu" << endl
+         << endl;
+
+    int option;
+    cout << "Enter your option: " << endl
+         << "[1] Consult your schedule" << endl
+         << "[2] Consult Uc" << endl
+         << "[3] Consult students" << endl // consultar os estudantes em turma, ano e uc
+         << "[4] Make a request" << endl   // tipo do request
+         << "[5] Undo a request" << endl
+         << "[6] Consult your requests" << endl;
+    cin >> option;
+
+    clearScreen();
+
+    switch (option)
+    {
+    case 1:
+    {
+    }
+    case 2:
+    {
+    }
+    case 3:
+    {
+    }
+    case 4:
+    {
+    }
+    case 5:
+    {
+    }
+    case 6:
+    {
+    }
+    }
+}
+
+void App::adminMenu()
+{
+    cout << "Administrator menu" << endl
+         << endl;
+
+    int option;
+    cout << "Enter your option: " << endl
+         << "[1] Consult Uc" << endl       // consultar ucs com mais estudantes, turmas da uc
+         << "[2] Consult students" << endl // consultar o n de estudantes em pelo menos n ucs, turma, ano e uc
+         << "[3] Consult requests" << endl;
+    cin >> option;
+
+    clearScreen();
+
+    switch (option)
+    {
+    case 1:
+    {
+    }
+    case 2:
+    {
+    }
+    case 3:
+    {
+    }
+    }
+}
