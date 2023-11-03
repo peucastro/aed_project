16445ed17f4ef7fb950e26c4bcf2f12aafaaeec3
Pedro Castro <falecompedroac@gmail.com>
2023-11-03 20:02:54 +0000

    adicionei as complexidades da classe request

--- inc/Request.hpp
+++ inc/Request.hpp
@@ -25,21 +25,24 @@ public:
     Request(std::string studentCode, char type);
 
     /**
-     * @brief Adds a student to a specified course and class.
+     * @brief Adds a student to a specified course and class.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param ucCodeDestination The code of the course to add the student.
      * @return True if the student is successfully added, false otherwise.
      */
     bool addUc(std::string ucCodeDestination);
 
     /**
-     * @brief Removes a student from a specified course.
+     * @brief Removes a student from a specified course.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param ucCode The code of the course to remove the student from.
      * @return True if the student is successfully removed, false otherwise.
      */
     bool removeUc(std::string ucCode);
 
     /**
-     * @brief Switches a student from one course to another, preserving their schedule.
+     * @brief Switches a student from one course to another, preserving their schedule.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param ucOrigin The original course code.
      * @param ucDestination The destination course code.
      * @return True if the student's course is successfully switched, false otherwise.
@@ -47,7 +50,8 @@ public:
     bool switchUc(std::string ucOrigin, std::string ucDestination);
 
     /**
-     * @brief Switches a student from one class to another within the same course.
+     * @brief Switches a student from one class to another within the same course.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param uc The course code.
      * @param classOrigin The original class code.
      * @param classDestination The destination class code.
@@ -56,24 +60,28 @@ public:
     bool switchClass(std::string uc, std::string classOrigin, std::string classDestination);
 
     /**
-     * @brief Displays the list of requests for a specific student.
+     * @brief Displays the list of requests for a specific student.\n
+     * Time complexity: O(N), where N is the number of lines in the requests_log.csv file.
      * @param studentCode The student's unique code.
      */
     void studentRequests(const std::string &studentCode);
 
-    /** @brief Displays all admin requests. */
+    /** @brief Displays all requests.\n
+     *  Time complexity: O(N), where N is the number of lines in the requests_log.csv file.
+     */
     void adminRequests();
 
     /**
-     * @brief Undoes a specific request by its ID.
+     * @brief Undoes a specific request by its ID.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param id The ID of the request to undo.
      */
     void undoRequest(unsigned id);
 
     /**
-     * @brief Checks the eligibility of available classes for a student's UC request.
-     *
-     * It evaluates if the student can be assigned to a class without violating capacity, balance, and schedule constraints.
+     * @brief Checks the eligibility of available classes for a student's UC request.\n
+     * It evaluates if the student can be assigned to a class without violating capacity, balance, and schedule constraints.\n
+     * Time complexity: O(N²), where N is the number of lines in the classes.csv file.
      *
      * @param ucDestination The UC code for which the student is requesting enrollment.
      * @param eligibleClasses A queue containing eligible class codes for the student's request.
--- inc/Script.hpp
+++ inc/Script.hpp
@@ -17,81 +17,81 @@ class Script
 {
 public:
     /**
-     * @brief Loads a student based on the student code.
+     * @brief Loads a student based on the student code.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param studentCode The student code to load.
      * @return The loaded Student object.
-     * @details Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      */
     Student loadStudent(const std::string &studentCode);
 
     /**
-     * @brief Loads a list of lectures for a specific UC and class code.
+     * @brief Loads a list of lectures for a specific UC and class code.\n
+     * Time complexity: O(N), where N is the number of lines in the classes.csv file.
      * @param ucCode_ The UC code.
      * @param classCode_ The class code.
      * @return A list of loaded Lecture objects.
-     * @details Time complexity: O(N), where N is the number of lines in the classes.csv file.
      */
     std::list<Lecture> loadLecture(std::string ucCode_, std::string classCode_);
 
     /**
-     * @brief Loads classes into a Uc object.
+     * @brief Loads classes into a Uc object.\n
+     * Time complexity: O(N), where N is the number of lines in the classes_per_uc.csv file.
      * @param uc_ The Uc object to load classes into.
-     * @details Time complexity: O(N), where N is the number of lines in the classes_per_uc.csv file.
      */
     void loadClasses(Uc &uc_);
 
     /**
-     * @brief Populates a lecture with students who are enrolled in it.
+     * @brief Populates a lecture with students who are enrolled in it.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param oneLecture_ The lecture to populate with students.
-     * @details Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      */
     void studentsInLecture(Lecture &oneLecture_);
 
     /**
-     * @brief Gets the schedule of lectures for a student based on their student code.
+     * @brief Gets the schedule of lectures for a student based on their student code.\n
+     * Time complexity: O(N), where N is the number of lines in the classes.csv file.
      * @param studentCode_ The student code.
      * @return A set of Lecture objects representing the student's schedule.
-     * @details Time complexity: O(N), where N is the number of lines in the classes.csv file.
      */
     std::set<Lecture> getSchedule(const std::string &studentCode);
 
     /**
-     * @brief Retrieves a list of students enrolled in a specific UC.
+     * @brief Retrieves a list of students enrolled in a specific UC.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param uc The Uc object representing the UC.
      * @return A vector of Student objects.
-     * @details Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      */
     std::vector<Student> studentsinUc(Uc &uc);
 
     /**
-     * @brief Retrieves a list of students enrolled in a specific class.
+     * @brief Retrieves a list of students enrolled in a specific class.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param ucCode_ The UC code.
      * @param classCode_ The class code.
      * @return A vector of Student objects.
-     * @details Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      */
     std::vector<Student> studentsinClass(std::string ucCode_, std::string classCode_);
 
     /**
-     * @brief Retrieves a set of students based on their enrollment year.
+     * @brief Retrieves a set of students based on their enrollment year.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param year The year for which to retrieve students.
      * @return An unordered set of Student objects.
-     * @details Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      */
     std::unordered_set<Student, Student::Hash> studentsInYear(const std::string &year);
 
     /**
-     * @brief Counts the number of students enrolled in at least 'number' UCs.
+     * @brief Counts the number of students enrolled in at least 'number' UCs.\n
+     * Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      * @param number The minimum number of UCs for a student to be counted.
      * @return The count of students meeting the criteria.
-     * @details Time complexity: O(N), where N is the number of lines in the students_classes.csv file.
      */
     int studentsInNUc(int number);
 
     /**
-     * @brief Retrieves a list of UCs with the most students, along with the number of students in each UC.
+     * @brief Retrieves a list of UCs with the most students, along with the number of students in each UC.\n
+     * Time complexity: O(N log N), where N is the number of lines in the students_classes.csv file.
      * @return A vector of pairs, where the first element is the UC code, and the second element is the number of students.
-     * @details Time complexity: O(N log N), where N is the number of lines in the students_classes.csv file.
      */
     std::vector<std::pair<std::string, int>> ucsWithMostStudents();
 };
